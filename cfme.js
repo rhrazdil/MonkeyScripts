6eabf0da94997c3916d1e5f7d8f24891eaba10b1
rhrazdil <32546791+rhrazdil@users.noreply.github.com>
2018-10-15 08:37:20 +0200

    Created cfme.js
    Fills email and random VM name fields upon clicking them.

---
+++ cfme.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name CFME provision VM speed button
+// @namespace Violentmonkey Scripts
+// @match https://*/*
+// @grant GM_addStyle
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js 
+// ==/UserScript==
+
+
+var path = location.pathname;
+
+function generate_string(prefix){
+  return prefix + Math.random().toString(36).substring(7);
+}
+
+switch (true) {
+  case path.includes('vm_infra/explorer'):
+    $(document).ready(function() {
+      $(document).on("click", function(event) {
+          if (event.target.id == 'requester__owner_email'){
+            $("#requester__owner_email").val('test_email@domain.com');
+          } else if (event.target.id == 'service__vm_name'){
+            $("#service__vm_name").val(generate_string('test_vm'));
+          }
+      });
+    });
+    break;
+};
